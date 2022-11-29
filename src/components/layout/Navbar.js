1170b085efb80fdff6eafbdf2593b591460b7c27
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-11-29 16:21:32 +0700

    Update title

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-primary">
-        Hello TCD0501
+        <h1>GitHub Finder</h1>
         <ul>
           <li>
             <Link to="/">Home</Link>
